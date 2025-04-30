3447f5132fb7ca5591414066da9e4af28a3edb2e
Nikolaj J-K <kagenogmig@gmail.com>
2025-04-30 20:23:52 +0200

    Deno problems
    I think Deno has become a problem and it should be changed to default Node. I am following the instructions, but I cannot import tfjs. At least the function tf.setBackend does not exist, but I have set the library in package.json and ran deno install. An error is thrown because no backend exist.

--- blade/src/main.ts
+++ blade/src/main.ts
@@ -1,12 +1,25 @@
-import './style.css'
-import typescriptLogo from '/typescript.svg'
-import viteLogo from '/vite.svg'
+import "./style.css";
+import typescriptLogo from "/typescript.svg";
+import viteLogo from "/vite.svg";
+import * as tf from "npm:@tensorflow/tfjs";
+import { loadGraphModel } from '@tensorflow/tfjs-converter';
+
+tf.setBackend('weblgl');
+console.log(tf);
+
+const MODEL_URL = '/web_model/model.json';
+
+const model = await loadGraphModel(MODEL_URL);
 
 const video = document.querySelector("video");
 const constraints = {
   audio: false,
   video: true,
 };
+
+setTimeout(() => {
+  model.execute(tf.browser.fromPixels(video));
+}, 1000);
 
 navigator.mediaDevices
   .getUserMedia(constraints)
